7a45d779cbb4f4ec430c4e705eb2439ad700feb0
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-09-17 14:42:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import fastify from 'fastify';
 import { processRequest } from './src/proxy.js'; // Import the named export
 
 const app = fastify({ 
-  logger: true,
+  logger: false,
   disableRequestLogging: true,
   trustProxy: true // Enable trust proxy
 });
