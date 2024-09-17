c4024678a408ef39018b94dbae29b72d4914a7a7
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-09-17 14:41:30 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -3,29 +3,27 @@ import sharp from 'sharp';
 import { redirect } from './redirect.js';
 
 export async function compressImg(request, reply, input) {
-    const { webp, grayscale, quality, originSize } = request.params;
-    const imgFormat = webp ? 'webp' : 'jpeg';
+    const format = request.params.webp ? 'webp' : 'jpeg'
 
     try {
         const sharpInstance = sharp()
-            .grayscale(grayscale)
-            .toFormat(imgFormat, {
-                quality,
+            .grayscale(request.params.grayscale)
+            .toFormat(format, {
+                quality: request.params.quality,
                 progressive: true,
-                optimizeScans: true,
-                chromaSubsampling: '4:4:4'
+                optimizeScans: true
             });
 
-        // Pipe the stream from input.body into the sharp instance
-        input.body.pipe(sharpInstance);
-
-        const { data, info } = await sharpInstance.toBuffer({ resolveWithObject: true });
+        // Pipe the input stream into the Sharp instance and convert it to a buffer
+        const { data, info } = await input.body
+            .pipe(sharpInstance)
+            .toBuffer({ resolveWithObject: true });
 
         reply
-            .header('content-type', `image/${imgFormat}`)
+            .header('content-type', 'image/' + format)
             .header('content-length', info.size)
-            .header('x-original-size', originSize)
-            .header('x-bytes-saved', originSize - info.size)
+            .header('x-original-size', request.params.originSize)
+            .header('x-bytes-saved', request.params.originSize - info.size)
             .code(200)
             .send(data);
     } catch (error) {
